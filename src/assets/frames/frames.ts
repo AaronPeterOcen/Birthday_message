cc45f3fbc54b0479b78bd5b59decfadacd0f4ec9
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2026-01-09 20:22:28 +0300

    first commit

---
+++ src/assets/frames/frames.ts
@@ -0,0 +1,7 @@
+// assets/frames.ts
+import frame1 from "./frame1.jpg";
+import frame2 from "./frame2.jpg";
+import frame3 from "./frame3.jpg";
+import frame4 from "./frame4.jpg";
+
+export { frame1, frame2, frame3, frame4 };
